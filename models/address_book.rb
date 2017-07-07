c9e4cb5e4c30c660dccc1955c0dddca215bd1ca2
Bill <bill@pcrsocial.com>
2017-07-07 09:54:10 -0400

    checkpoint 7 done

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,7 +44,29 @@ def remove_entry(name, phone_number, email)
     end 
 
   @entries.delete(delete_entry)
+end
+
+def binary_search(name)
+  lower = 0
+  upper = entries.length - 1
+
+  while lower <= upper 
+    mid = (lower + upper) / 2
+    mid_name = entries[mid].name
+
+    if name == mid_name 
+      return entries[mid]
+    elsif name < mid_name 
+      upper = mid - 1
+    elsif name > mid_name
+      lower = mid + 1
+    end
+  end
+
+  return nil
 
 end
+
+
 
 end
